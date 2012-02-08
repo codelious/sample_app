c89d0bc49292de015b5a94d28438a343d6835090
Mario Espinoza <mario@magrocode.com>
2012-02-07 23:43:39 -0300

    hasta metodo de autenticacion 7.2.4

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,16 +25,27 @@ class User < ActiveRecord::Base
   def has_password?(submitted_password)
     # compare encrypted_password with the encrypted version of
     # submitted password
+    encrypted_password == encrypt(submitted_password)
   end
   
   private
   
     def encrypt_password
+      self.salt = make_salt unless has_password?(password)
       self.encrypted_password = encrypt(password)
     end
     
     def encrypt(string)
-      string # solamente una implementacion temporal
+      # solamente una implementacion temporal
+      secure_hash("#{salt}--#{string}")
+    end
+    
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{password}")
+    end
+    
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
     end
   
 end
