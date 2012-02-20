ddef54560b4ba546ce2a19bb505bd27bedc85777
Mario Espinoza <mario@magrocode.com>
2012-02-19 23:35:48 -0300

    Empezando el signin

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -9,32 +9,41 @@ describe "Paginas de usuario" do
     
     before { visit signup_path }
     
-    describe "con informacion invalida" do
-      it "no debe crear un usuario" do
-        expect {click_button "Sign up" }.not_to change(User, :count)
+      describe "con informacion invalida" do
+        it "no debe crear un usuario" do
+          expect {click_button "Sign up" }.not_to change(User, :count)
+        end
       end
-    end
     
-    describe "con informacion valida" do
-      before do
-        fill_in "Name", with: "Usuario Ejemplo"
-        fill_in "Email", with: "user@example.com"
-        fill_in "Password", with: "foobar"
-        fill_in "Confirmation", with: "foobar"
+      describe "error messages" do
+        before { click_button "Sign up" }
+
+        let(:error) { 'errores prohiben que este usuario se guarde' }
+
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content(error) }
       end
-      
-      it "debe crear un usuario" do
-        expect { click_button "Sign up" }.to change(User, :count).by(1)
+            
+      describe "con informacion valida" do
+        before do
+          fill_in "Name", with: "Usuario Ejemplo"
+          fill_in "Email", with: "user@example.com"
+          fill_in "Password", with: "foobar"
+          fill_in "Confirmation", with: "foobar"
+        end
+        
+        it "debe crear un usuario" do
+          expect { click_button "Sign up" }.to change(User, :count).by(1)
+        end
+        
+        describe "despues de guardar el usuario" do
+          before { click_button "Sign up" }
+          let(:user) { User.find_by_email('user@example.com') }
+          
+          it { should have_selector('title', :text => user.name) }
+          it { should have_selector('div.flash.success', :text => 'Bienvenido')}
+          
+        end
       end
-      
-      #describe "despues de guardar el usuario" do
-      #  before { click_button "Sign up" }
-      #  let(:user) { User.find_by_email('user@example.com') }
-      #  
-      #  it { should have_selector('title', text: user.name) }
-      #  it { should have_selector('div.flash.success', text: 'Bienvenido')}
-      #  
-      #end
-    end
   end
 end
